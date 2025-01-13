10bd4c21ad2ec83d4f67b228e0cff7a3b4e29861
ItsAltoo <Itsmalik57@gmail.com>
2025-01-13 13:53:19 +0800

    13.1.25

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./tailwind/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+      polar: ["Polar snow", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
